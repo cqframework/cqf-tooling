9076bd45d048bcad8c0202850006c66da9e02477
jreyno77 <inujosha1@gmail.com>
2019-12-01 18:37:47 -0700

    changed id to <name>-<version> (for main library id and referenced library ids) changed output file name to id

--- src/main/java/org/opencds/cqf/library/STU3LibraryGenerator.java
+++ src/main/java/org/opencds/cqf/library/STU3LibraryGenerator.java
@@ -28,23 +28,12 @@ public class STU3LibraryGenerator extends BaseLibraryGenerator<Library, STU3Narr
         Library library = populateMeta(id, elm.getIdentifier().getVersion());
         if (elm.getIncludes() != null && !elm.getIncludes().getDef().isEmpty()) {
             for (IncludeDef def : elm.getIncludes().getDef()) {
-                if (!libraryMap.containsKey(def.getPath())) {
-                    if (!translatorMap.containsKey(def.getPath())) {
-                        throw new IllegalArgumentException("Referenced library: " + def.getPath().replaceAll("_", "-").toLowerCase() + " not found");
-                    }
-                    processLibrary(def.getPath(), translatorMap.get(def.getPath()));
-                }
-
-                library.addRelatedArtifact(
-                        new RelatedArtifact()
-                                .setType(RelatedArtifact.RelatedArtifactType.DEPENDSON)
-                                .setResource(new Reference().setReference("Library/" + def.getPath().replaceAll("_", "-").toLowerCase()))
-                );
-
-                for (DataRequirement req : libraryMap.get(def.getPath()).getDataRequirement()) {
+                processRelatedArtifact(def);
+                addRelatedArtifact(library, def);
+                for (DataRequirement req : libraryMap.get(getIncludedLibraryName(def)).getDataRequirement()) {
                     library.addDataRequirement(req);
                 }
-            }
+            }        
         }
 
         resolveDataRequirements(library, translator);
@@ -53,12 +42,21 @@ public class STU3LibraryGenerator extends BaseLibraryGenerator<Library, STU3Narr
         libraryMap.put(id, library);
     }
 
+    private void processRelatedArtifact(IncludeDef def) {
+        if (!libraryMap.containsKey(getIncludedLibraryName(def))) {
+            if (!translatorMap.containsKey(getIncludedLibraryName(def))) {
+                throw new IllegalArgumentException("Referenced library: " + getIncludedLibraryId(def) + " not found");
+            }
+            processLibrary(getIncludedLibraryName(def), translatorMap.get(getIncludedLibraryName(def)));
+        }
+    }
+
     @Override
     public void output() {
         Bundle bundle = new Bundle();
 
         for (Map.Entry<String, Library> entry : libraryMap.entrySet()) {
-            try (FileOutputStream writer = new FileOutputStream(getOutputPath() + "/library-" + entry.getKey().replaceAll("_", "-").toLowerCase() + "." + encoding)) {
+            try (FileOutputStream writer = new FileOutputStream(getOutputPath() + "/" + createFileName(entry.getValue().getId(), encoding))) {
                 bundle.addEntry().setResource(entry.getValue()).setRequest(new Bundle.BundleEntryRequestComponent().setMethod(Bundle.HTTPVerb.PUT).setUrl("Library/" + entry.getValue().getId()));
                 writer.write(
                         encoding.equals("json")
@@ -68,19 +66,19 @@ public class STU3LibraryGenerator extends BaseLibraryGenerator<Library, STU3Narr
                 writer.flush();
             } catch (IOException e) {
                 e.printStackTrace();
-                throw new IllegalArgumentException("Error outputting library: " + entry.getKey());
+                throw new IllegalArgumentException("Error outputting library: " + entry.getValue().getId());
             }
-            try (FileOutputStream writer = new FileOutputStream(getOutputPath() + "/elm-" + entry.getKey().replaceAll("_", "-").toLowerCase() + "." + encoding))
+            try (FileOutputStream writer = new FileOutputStream(getOutputPath() + "/elm-" + createFileName(entry.getValue().getId(), encoding)))
             {
                 writer.write(elmMap.get(entry.getKey()).getBytes());
                 writer.flush();
             } catch (IOException e) {
                 e.printStackTrace();
-                throw new IllegalArgumentException("Error outputting elm for library: " + entry.getKey());
+                throw new IllegalArgumentException("Error outputting elm for library: " + entry.getValue().getId());
             }
         }
 
-        try (FileOutputStream writer = new FileOutputStream(getOutputPath() + "/all-libraries-bundle." +  encoding)) {
+        try (FileOutputStream writer = new FileOutputStream(getOutputPath() + "/libraries-master-bundle." +  encoding)) {
             writer.write(
                     encoding.equals("json")
                             ? fhirContext.newJsonParser().setPrettyPrint(true).encodeResourceToString(bundle).getBytes()
@@ -96,13 +94,22 @@ public class STU3LibraryGenerator extends BaseLibraryGenerator<Library, STU3Narr
     // Populate metadata
     private Library populateMeta(String name, String version) {
         Library library = new Library();
-        library.setId(nameToId(name));
+        library.setId(nameToId(name, version));
         library.setName(name);
         library.setVersion(version);
         library.setStatus(Enumerations.PublicationStatus.ACTIVE);
         library.setExperimental(true);
         library.setType(new CodeableConcept().addCoding(new Coding().setCode("logic-library").setSystem("http://hl7.org/fhir/codesystem-library-type.html")));
         return library;
+    }
+
+    // Add Related Artifact
+    private void addRelatedArtifact(Library library, IncludeDef def) {
+        library.addRelatedArtifact(
+                new RelatedArtifact()
+                        .setType(RelatedArtifact.RelatedArtifactType.DEPENDSON)
+                        .setResource(new Reference().setReference("Library/" + getIncludedLibraryId(def))) //this is the reference name
+        );
     }
 
     // Resolve DataRequirements
@@ -145,7 +152,27 @@ public class STU3LibraryGenerator extends BaseLibraryGenerator<Library, STU3Narr
         return null;
     }
 
+    //helpers
+    private String getIncludedLibraryId(IncludeDef def) {
+        String name = getIncludedLibraryName(def);
+        String version = def.getVersion();
+        return nameToId(name, version);
+    }
+
+    private String getIncludedLibraryName(IncludeDef def) {
+        return def.getPath();
+    }
+
     private String nameToId(String name) {
         return name.replaceAll("_", "-").toLowerCase();
+    }
+
+    private String nameToId(String name, String version) {
+        String nameAndVersion = name + "-" + version;
+        return nameAndVersion.replaceAll("_", "-");
+    }
+
+    private String createFileName(String id, String encoding) {
+        return id + "." + encoding;
     }
 }
