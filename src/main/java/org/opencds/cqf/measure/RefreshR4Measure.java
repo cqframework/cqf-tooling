206e07e93f79d0747a11c7d3abf3c2ec8fe1e550
Chris Schuler <cschuler@myharmoniq.com>
2019-12-05 19:42:54 -0700

    Setting up the refresh generated content measure operation

---
+++ src/main/java/org/opencds/cqf/measure/RefreshR4Measure.java
@@ -0,0 +1,13 @@
+package org.opencds.cqf.measure;
+
+public class RefreshR4Measure extends RefreshGeneratedContent {
+
+    public RefreshR4Measure() {
+        super("src/main/resources/org/opencds/cqf/measure/output/r4", "-RefreshR4Measure");
+    }
+
+    @Override
+    public void refreshGeneratedContent() {
+
+    }
+}
