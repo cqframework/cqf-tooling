fa7cfacec41f573514b4fac06f649ee5fa909cab
rob-reynolds <4z&ChMu6yGn%IL>
2019-12-06 17:03:15 -0700

    Fix merge mistake from prior commit

--- src/main/java/org/opencds/cqf/library/r4/NarrativeProvider.java
+++ src/main/java/org/opencds/cqf/library/r4/NarrativeProvider.java
@@ -4,7 +4,6 @@ import ca.uhn.fhir.context.FhirContext;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Narrative;
 import org.opencds.cqf.library.BaseNarrativeProvider;
-import org.opencds.cqf.library.stu3.NarrativeProvider;
 
 public class NarrativeProvider extends BaseNarrativeProvider<Narrative> {
 
