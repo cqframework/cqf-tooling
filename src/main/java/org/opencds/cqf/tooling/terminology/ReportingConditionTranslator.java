75f7f040e6ff695e6d09f286968b2f0769e1574d
msyphus <mark.syphus@gmail.com>
2020-09-25 14:51:34 -0600

    added second criteria and comments

--- src/main/java/org/opencds/cqf/tooling/terminology/ReportingConditionTranslator.java
+++ src/main/java/org/opencds/cqf/tooling/terminology/ReportingConditionTranslator.java
@@ -11,8 +11,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ReportingConditionTranslator extends Operation {
 
-    private static String pathToSource = "C:/Users/marks/Repos/DCG/cqf-tooling/src/main/java/org/opencds/cqf/tooling/terminology/demo-Utah.json"; // -pathtosource (-pts)
+    //Set this for quick testing, need to adjust this path to be used with command line comand from Main.java. Don't forget to personalize path if you're trying to test this.
+    private static String pathToSource = "C:/../DCG/cqf-tooling/src/main/java/org/opencds/cqf/tooling/terminology/demo-Utah.json"; // -pathtosource (-pts)
 
+    //Specifying command line commands
     @Override
     public void execute(final String[] args) {
         setOutputPath("src/main/resources/org/opencds/cqf/tooling/terminology/output"); // default
@@ -38,6 +40,7 @@ public class ReportingConditionTranslator extends Operation {
         } 
     } 
 
+    //Load the demo-UT JSON, pull negative criteria for Chlamydia (incomplete), and make concise JSON with only data we need for CQL.
     public static void main(String[] args) throws IOException {
         byte[] jsonData = Files.readAllBytes(Paths.get(pathToSource));
         ObjectMapper objectMapper = new ObjectMapper();
@@ -49,11 +52,17 @@ public class ReportingConditionTranslator extends Operation {
         JsonNode neg005Right = neg005.at("/predicates").get(0).at("/predicateParts").get(2).at("/predicatePartConcepts").get(0).at("/openCdsConceptDTO/displayName");
         JsonNode neg005LeftType = neg005.at("/predicates").get(1).at("/predicateParts").get(0).at("/partAlias");
         JsonNode neg005RightResult = neg005.at("/predicates").get(1).at("/predicateParts").get(2).at("/predicatePartConcepts").get(0).at("/openCdsConceptDTO/displayName"); 
+        JsonNode neg006 = chlamydia.at("/conditionCriteriaRels").get(23);
+        JsonNode neg006Label = neg006.at("/label");
+        JsonNode neg006Left = neg006.at("/predicates").get(0).at("/predicateParts").get(0).at("/partAlias");
+        JsonNode neg006Right = neg006.at("/predicates").get(0).at("/predicateParts").get(2).at("/predicatePartConcepts").get(0).at("/openCdsConceptDTO/displayName");
+        JsonNode neg006LeftType = neg006.at("/predicates").get(1).at("/predicateParts").get(0).at("/partAlias");
+        JsonNode neg006RightResult = neg006.at("/predicates").get(1).at("/predicateParts").get(2).at("/predicatePartConcepts").get(0).at("/openCdsConceptDTO/displayName"); 
         JsonNode lab = chlamydia.at("/logicSets").get(1).at("/name");
         JsonNode labReporting = chlamydia.at("/logicSets").get(2).at("/name");
         JsonNode jurisdictionCode = chlamydia.at("/responsibleAgencies").get(0).at("/jurisdictionIdentifier");
-        String neg005String = "{\"negativeCriteria\": [{\"label\": " + neg005Label + ", \"predicates\": [{\"case\": " + neg005Left + ", \"operator\": \"=\", \"condition\": " + neg005Right + "}, {\"conjunction\": \"and\"}, {\"case\": " + neg005LeftType + ", \"operator\": \"=\", \"condition\": " + neg005RightResult + "}], \"reporting\": [{\"facility\": [{\"type\": " + lab + "}, {\"type\": " + labReporting + "}]}, {\"jurisdiction\": " + jurisdictionCode + "}]}]}]}";
-        JsonNode neg005UT = objectMapper.readTree(neg005String);
-        objectMapper.writeValue(new File("updatedCondition.json"), neg005UT);
+        String negString = "{\"negativeCriteria\": [{\"label\": " + neg005Label + ", \"predicates\": [{\"case\": " + neg005Left + ", \"operator\": \"=\", \"condition\": " + neg005Right + "}, {\"conjunction\": \"and\"}, {\"case\": " + neg005LeftType + ", \"operator\": \"=\", \"condition\": " + neg005RightResult + "}], \"reporting\": [{\"facility\": [{\"type\": " + lab + "}, {\"type\": " + labReporting + "}]}, {\"jurisdiction\": " + jurisdictionCode + "}]}, {\"label\": " + neg006Label + ", \"predicates\": [{\"case\": " + neg006Left + ", \"operator\": \"=\", \"condition\": " + neg006Right + "}, {\"conjunction\": \"and\"}, {\"case\": " + neg006LeftType + ", \"operator\": \"=\", \"condition\": " + neg006RightResult + "}], \"reporting\": [{\"facility\": [{\"type\": " + lab + "}, {\"type\": " + labReporting + "}]}, {\"jurisdiction\": " + jurisdictionCode + "}]}]}";
+        JsonNode negUT = objectMapper.readTree(negString);
+        objectMapper.writeValue(new File("updatedCondition.json"), negUT);
     }
 }
