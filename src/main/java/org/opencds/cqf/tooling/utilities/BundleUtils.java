1dbdd0a78be9c8a00a677d2d003ee8825e6bcfb6
Adam Stevenson <stevenson_adam@yahoo.com>
2020-10-07 20:14:45 -0600

    Modified testIG operation to stamp output artifacts with ruler stamp.

--- src/main/java/org/opencds/cqf/tooling/utilities/BundleUtils.java
+++ src/main/java/org/opencds/cqf/tooling/utilities/BundleUtils.java
@@ -2,12 +2,14 @@ package org.opencds.cqf.tooling.utilities;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.RuntimeResourceDefinition;
+import org.hl7.fhir.instance.model.api.IBase;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.opencds.cqf.tooling.common.CqfmSoftwareSystem;
+import org.w3._1999.xhtml.P;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BundleUtils {
@@ -81,24 +83,57 @@ public class BundleUtils {
         }
     }
 
-    public static List<IBaseResource> GetBundlesInDir(String directoryPath, FhirContext fhirContext) {
+    public static List<Map.Entry<String, IBaseResource>> GetBundlesInDir(String directoryPath, FhirContext fhirContext) {
         return GetBundlesInDir(directoryPath, fhirContext, true);
     }
 
-    public static List<IBaseResource> GetBundlesInDir(String directoryPath, FhirContext fhirContext, Boolean recursive) {
+    public static List<Map.Entry<String, IBaseResource>> GetBundlesInDir(String directoryPath, FhirContext fhirContext, Boolean recursive) {
         File dir = new File(directoryPath);
         if (!dir.isDirectory()) {
             throw new IllegalArgumentException("path to directory must be an existing directory.");
         }
 
         List<String> filePaths = IOUtils.getFilePaths(directoryPath, recursive).stream().filter(x -> !x.endsWith(".cql")).collect(Collectors.toList());
-        List<IBaseResource> resources = IOUtils.readResources(filePaths, fhirContext);
 
+        List<Map.Entry<String, IBaseResource>> bundleMap = new ArrayList<>();
         RuntimeResourceDefinition bundleDefinition = (RuntimeResourceDefinition)ResourceUtils.getResourceDefinition(fhirContext, "Bundle");
         String bundleClassName = bundleDefinition.getImplementingClass().getName();
-        return resources.stream()
-            .filter(entry -> entry != null)
-            .filter(entry ->  bundleClassName.equals(entry.getClass().getName()))
-            .collect(Collectors.toList());
+        for (String path : filePaths) {
+            IBaseResource resource = IOUtils.readResource(path, fhirContext);
+            if (resource != null) {
+                if (bundleClassName.equals(resource.getClass().getName())) {
+                    Map.Entry<String, IBaseResource> bundleEntry = new AbstractMap.SimpleEntry<>(path, resource);
+                    bundleMap.add(bundleEntry);
+                }
+            }
+        }
+
+        return bundleMap;
+//        List<IBaseResource> resources = IOUtils.readResources(filePaths, fhirContext);
+//
+//        return resources.stream()
+//            .filter(entry -> entry != null)
+//            .filter(entry ->  bundleClassName.equals(entry.getClass().getName()))
+//            .collect(Collectors.toList());
+    }
+
+    public static void stampDstu3BundleEntriesWithSoftwareSystems(org.hl7.fhir.dstu3.model.Bundle bundle, List<CqfmSoftwareSystem> softwareSystems, FhirContext fhirContext, String rootDir) {
+        for (org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent entry: bundle.getEntry()) {
+            org.hl7.fhir.dstu3.model.Resource resource = entry.getResource();
+            if ((resource.fhirType().equals("Library")) || ((resource.fhirType().equals("Measure")))) {
+                org.opencds.cqf.tooling.common.stu3.CqfmSoftwareSystemHelper cqfmSoftwareSystemHelper = new org.opencds.cqf.tooling.common.stu3.CqfmSoftwareSystemHelper(rootDir);
+                cqfmSoftwareSystemHelper.ensureSoftwareSystemExtensionAndDevice((org.hl7.fhir.dstu3.model.DomainResource)resource, softwareSystems, fhirContext);
+            }
+        }
+    }
+
+    public static void stampR4BundleEntriesWithSoftwareSystems(org.hl7.fhir.r4.model.Bundle bundle, List<CqfmSoftwareSystem> softwareSystems, FhirContext fhirContext, String rootDir) {
+        for (org.hl7.fhir.r4.model.Bundle.BundleEntryComponent entry: bundle.getEntry()) {
+            org.hl7.fhir.r4.model.Resource resource = entry.getResource();
+            if ((resource.fhirType().equals("Library")) || ((resource.fhirType().equals("Measure")))) {
+                org.opencds.cqf.tooling.common.r4.CqfmSoftwareSystemHelper cqfmSoftwareSystemHelper = new org.opencds.cqf.tooling.common.r4.CqfmSoftwareSystemHelper(rootDir);
+                cqfmSoftwareSystemHelper.ensureSoftwareSystemExtensionAndDevice((org.hl7.fhir.r4.model.DomainResource)resource, softwareSystems, fhirContext);
+            }
+        }
     }
 }
