0728b6273edb047c0600b535e8d9c5faacf11b3c
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2020-11-20 12:12:26 -0700

    #160: Added or corrected primaryCodePath for some R4 resources

--- src/main/java/org/opencds/cqf/tooling/modelinfo/fhir/FHIRClassInfoSettings.java
+++ src/main/java/org/opencds/cqf/tooling/modelinfo/fhir/FHIRClassInfoSettings.java
@@ -90,10 +90,11 @@ class FHIRClassInfoSettings extends ClassInfoSettings {
             {
                 put("Account", "type");
                 put("ActivityDefinition", "topic");
-                put("AdverseEvent", "type");
+                put("AdverseEvent", "event");
                 put("AllergyIntolerance", "code");
                 put("Appointment", "serviceType");
                 put("Basic", "code");
+                put("BodyStructure", "location");
                 put("CarePlan", "category");
                 put("CareTeam", "category");
                 put("ChargeItemDefinition", "code");
@@ -105,10 +106,10 @@ class FHIRClassInfoSettings extends ClassInfoSettings {
                 put("Condition", "code");
                 put("Consent", "category");
                 put("Coverage", "type");
-                put("DetectedIssue", "category");
+                put("DetectedIssue", "code");
                 put("Device", "type");
                 put("DeviceMetric", "type");
-                put("DeviceRequest", "codeCodeableConcept");
+                put("DeviceRequest", "code");
                 put("DeviceUseStatement", "device.code");
                 put("DiagnosticReport", "code");
                 put("Encounter", "type");
@@ -120,6 +121,7 @@ class FHIRClassInfoSettings extends ClassInfoSettings {
                 put("HealthcareService", "type");
                 put("Immunization", "vaccineCode");
                 put("Library", "topic");
+                put("Location", "type");
                 put("Measure", "topic");
                 put("MeasureReport", "measure.topic");
                 put("Medication", "code");
@@ -130,10 +132,12 @@ class FHIRClassInfoSettings extends ClassInfoSettings {
                 put("MessageDefinition", "event");
                 put("Observation", "code");
                 put("OperationOutcome", "issue.code");
+                put("PractitionerRole", "code");
                 put("Procedure", "code");
                 put("ProcedureRequest", "code");
                 put("Questionnaire", "name");
                 put("ReferralRequest", "type");
+                put("RelatedPerson", "relationship");
                 put("RiskAssessment", "code");
                 put("SearchParameter", "target");
                 put("Sequence", "type");
