96797a03c2823ec2220e9d16d857cf4fbf09b06b
Jonathan Percival <jonathan.i.percival@gmail.com>
2021-02-16 13:42:17 -0700

    Fix failing test

--- src/test/java/org/opencds/cqf/tooling/terminology/CMSFlatMultiValueSetGeneratorTests.java
+++ src/test/java/org/opencds/cqf/tooling/terminology/CMSFlatMultiValueSetGeneratorTests.java
@@ -3,7 +3,6 @@ package org.opencds.cqf.tooling.terminology;
 import org.junit.Test;
 
 import java.io.File;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class CMSFlatMultiValueSetGeneratorTests {
@@ -13,13 +12,13 @@ public class CMSFlatMultiValueSetGeneratorTests {
         VSACBatchValueSetGenerator generator = new VSACBatchValueSetGenerator();
         generator.execute(new String[] {
                 "-VsacXlsxToValueSetBatch",
-                "-ptsd=\"src\\test\\resources\\org\\opencds\\cqf\\tooling\\terminology\"",
-                "-op=\"src\\test\\resources\\org\\opencds\\cqf\\tooling\\terminology\\output\"",
+                "-ptsd=src/test/resources/org/opencds/cqf/tooling/terminology",
+                "-op=target/test/resources/org/opencds/cqf/tooling/terminology/output",
                 "-setname=true",
                 "-vssrc=cms"
         });
 
-        File outputPath = Paths.get("src\\test\\resources\\org\\opencds\\cqf\\tooling\\terminology\\output").toFile();
+        File outputPath = Paths.get("target/test/resources/org/opencds/cqf/tooling/terminology/output").toFile();
         int fileCount = 0;
         for (File outputFile : outputPath.listFiles()) {
             fileCount++;
