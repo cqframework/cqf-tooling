5df11417a18073b38e5bee9ce8d1683c55bc12c6
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2021-03-14 15:08:17 -0600

    #207: Added support for well-known model and library resolution

--- src/main/java/org/opencds/cqf/tooling/npm/NpmLibrarySourceProvider.java
+++ src/main/java/org/opencds/cqf/tooling/npm/NpmLibrarySourceProvider.java
@@ -31,13 +31,26 @@ public class NpmLibrarySourceProvider implements LibrarySourceProvider {
         // VersionedIdentifier.id: Name of the library
         // VersionedIdentifier.system: Namespace for the library, as a URL
         // VersionedIdentifier.version: Version of the library
+
         for (NpmPackage p : packages) {
             try {
-                InputStream s = p.loadByCanonicalVersion(identifier.getSystem()+"/Library/"+identifier.getId(), identifier.getVersion());
+                VersionedIdentifier libraryIdentifier = new VersionedIdentifier()
+                        .withId(identifier.getId())
+                        .withVersion(identifier.getVersion())
+                        .withSystem(identifier.getSystem());
+
+                if (libraryIdentifier.getSystem() == null) {
+                    libraryIdentifier.setSystem(p.canonical());
+                }
+
+                InputStream s = p.loadByCanonicalVersion(libraryIdentifier.getSystem()+"/Library/"+libraryIdentifier.getId(), libraryIdentifier.getVersion());
                 if (s != null) {
                     Library l = reader.readLibrary(s);
                     for (org.hl7.fhir.r5.model.Attachment a : l.getContent()) {
                         if (a.getContentType() != null && a.getContentType().equals("text/cql")) {
+                            if (identifier.getSystem() == null) {
+                                identifier.setSystem(libraryIdentifier.getSystem());
+                            }
                             return new ByteArrayInputStream(a.getData());
                         }
                     }
--- src/main/java/org/opencds/cqf/tooling/processor/CqlProcessor.java
+++ src/main/java/org/opencds/cqf/tooling/processor/CqlProcessor.java
@@ -261,7 +261,7 @@ public class CqlProcessor {
         ModelManager modelManager = new ModelManager();
         LibraryManager libraryManager = new LibraryManager(modelManager);
         if (packages != null) {
-            modelManager.getModelInfoLoader().registerModelInfoProvider(new NpmModelInfoProvider(packages, reader, logger));
+            modelManager.getModelInfoLoader().registerModelInfoProvider(new NpmModelInfoProvider(packages, reader, logger), true);
             libraryManager.getLibrarySourceLoader().registerProvider(new NpmLibrarySourceProvider(packages, reader, logger));
         }
         libraryManager.getLibrarySourceLoader().registerProvider(new FhirLibrarySourceProvider());
@@ -471,9 +471,22 @@ public class CqlProcessor {
                 .setResource(getModelInfoReferenceUrl(usingDef.getUri(), usingDef.getLocalIdentifier(), usingDef.getVersion()));
     }
 
+    /*
+    Override the referencing URL for the FHIR-ModelInfo library
+    This is required because models do not have a "namespace" in the same way that libraries do,
+    so there is no way for the UsingDefinition to have a Uri that is different than the expected URI that the
+    providers understand. I.e. model names and model URIs are one-to-one.
+     */
+    private String mapModelInfoUri(String uri, String name) {
+        if (name.equals("FHIR") && uri.equals("http://hl7.org/fhir")) {
+            return "http://fhir.org/guides/cqf/common";
+        }
+        return uri;
+    }
+
     private String getModelInfoReferenceUrl(String uri, String name, String version) {
         if (uri != null) {
-            return String.format("%s/Library/%s-ModelInfo%s", uri, name, version != null ? ("|" + version) : "");
+            return String.format("%s/Library/%s-ModelInfo%s", mapModelInfoUri(uri, name), name, version != null ? ("|" + version) : "");
         }
 
         return String.format("Library/%-ModelInfo%s", name, version != null ? ("|" + version) : "");
