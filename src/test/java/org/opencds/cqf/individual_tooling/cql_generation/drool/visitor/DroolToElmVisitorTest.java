f264ae0e9ed3e2554ff76e240c4c6252cb7a4e03
Jreyno77 <jreysteven1@gmail.com>
2021-04-21 15:03:24 -0600

    Setting tests back up

--- src/test/java/org/opencds/cqf/individual_tooling/cql_generation/drool/visitor/DroolToElmVisitorTest.java
+++ src/test/java/org/opencds/cqf/individual_tooling/cql_generation/drool/visitor/DroolToElmVisitorTest.java
@@ -54,14 +54,13 @@ public class DroolToElmVisitorTest {
     @Test
     public void EvaluatePatienthasadiagnosisof() {
         setup("[]_Thrombocytopenia_UPDATED_TEMPLATE_5477e.xml");
-        List<String> expressions = readFileInList(
-                "../CQLGenerationDocs/GeneratedDocs/elm/expressions/[]_Thrombocytopenia__Expression.txt");
+        List<String> expressions = readFileInList("../CQLGenerationDocs/GeneratedDocs/elm/expressions/[]_Thrombocytopenia__Expression.txt");
         List<Object> results = new ArrayList<Object>();
         for (String expression : expressions) {
-            //String observationInterpretationRelatedExpressions = "(?i)Group 1.1-d5acd129f47353d9de23d4f5c54c4821|Group 2.2-bd2434cd92e9dc1f22b181256d29884b|Lab Result Interpretation-bcfb9161a92a6b6575cf9e6658816c12|Group 1-b2ec8fd8ef385f444209414cdd4f0bad|Lab Result Interpretation-459dc3e6db577f49f4221697c63ffec9|Group 2-5acef8b7bbc1bdea7569c54d5f407772|ConditionCriteriaMet";
-            // if (expression.matches(observationInterpretationRelatedExpressions)) {
-            //     System.out.println("Awaiting engine fix Observation Interpretation logic");
-            // } else {
+            String observationInterpretationRelatedExpressions = "(?i)Group 1.1-d5acd129f47353d9de23d4f5c54c4821|Group 2.2-bd2434cd92e9dc1f22b181256d29884b|Lab Result Interpretation-bcfb9161a92a6b6575cf9e6658816c12|Group 1-b2ec8fd8ef385f444209414cdd4f0bad|Lab Result Interpretation-459dc3e6db577f49f4221697c63ffec9|Group 2-5acef8b7bbc1bdea7569c54d5f407772|ConditionCriteriaMet";
+            if (expression.matches(observationInterpretationRelatedExpressions)) {
+                System.out.println("Awaiting engine fix Observation Interpretation logic");
+            } else {
                 if (expression.equals(
                         "Patient has lab result with test name (specific or an organism or substance)-3f12c73cbe886ab3f908544bb1ca8266")) {
                     System.out.println("Missing ValueSet: https://hln.com/fhir/ValueSet/VHF008");
@@ -80,7 +79,7 @@ public class DroolToElmVisitorTest {
                         e.printStackTrace();
                     }
                 }
-            //}
+            }
         }
         // generateElmForDebug();
 
@@ -89,11 +88,11 @@ public class DroolToElmVisitorTest {
 
     @Test
     public void test_ModelingK() {
-        // String xmlFileName = "GeneratedCql14_922c0.xml";
-        // String expressionListFilePath =
-        // "../CQLGenerationDocs/GeneratedDocs/elm/expressions/GeneratedCql14_922c0_Expression.txt";
-        // runAllExpressionsFromFile(xmlFileName, expressionListFilePath);
-        generateElmForDebug();
+        String xmlFileName = "GeneratedCql14_922c0.xml";
+        String expressionListFilePath =
+        "../CQLGenerationDocs/GeneratedDocs/elm/expressions/GeneratedCql14_922c0_Expression.txt";
+        runAllExpressionsFromFile(xmlFileName, expressionListFilePath);
+        //generateElmForDebug();
 
         // results.forEach(object -> Assert.assertTrue(((List<?>) object).size() == 2));
     }
@@ -110,33 +109,32 @@ public class DroolToElmVisitorTest {
 
     @Test
     public void test_ModelingN() {
-        // String xmlFileName = "Patient_is_deceased_84755.xml";
-        // String expressionListFilePath = "../CQLGenerationDocs/GeneratedDocs/elm/expressions/Patient_is_deceased__Expression.txt";
-        // runAllExpressionsFromFile(xmlFileName, expressionListFilePath);
-        generateElmForDebug();
-        System.out.println("test");
-
-        // results.forEach(object -> Assert.assertTrue(((List<?>) object).size() == 2));
-    }
-
-    @Test
-    public void test_ModelingC() {
-        String xmlFileName = "GeneratedCql136_89fdc.xml";
-        String expressionListFilePath = "../CQLGenerationDocs/GeneratedDocs/elm/expressions/GeneratedCql136_89fd_Expression.txt";
+        String xmlFileName = "Patient_is_deceased_84755.xml";
+        String expressionListFilePath = "../CQLGenerationDocs/GeneratedDocs/elm/expressions/Patient_is_deceased__Expression.txt";
         runAllExpressionsFromFile(xmlFileName, expressionListFilePath);
-        generateElmForDebug();
+        // generateElmForDebug();
+        // System.out.println("test");
 
         // results.forEach(object -> Assert.assertTrue(((List<?>) object).size() == 2));
     }
+
+    // @Test
+    // public void test_ModelingC() {
+    //     String xmlFileName = "GeneratedCql136_89fdc.xml";
+    //     String expressionListFilePath = "../CQLGenerationDocs/GeneratedDocs/elm/expressions/GeneratedCql136_89fd_Expression.txt";
+    //     runAllExpressionsFromFile(xmlFileName, expressionListFilePath);
+    //     //generateElmForDebug();
+
+    //     // results.forEach(object -> Assert.assertTrue(((List<?>) object).size() == 2));
+    // }
 
     @Test
     public void other() {
-        // String xmlFileName = "Abdominal_Cramps_314c4.xml";
-        // String expressionListFilePath =
-        // "../CQLGenerationDocs/GeneratedDocs/elm/expressions/Abdominal_Cramps_314_Expression.txt";
-        // runAllExpressionsFromFile(xmlFileName, expressionListFilePath);
+        String xmlFileName = "Abdominal_Cramps_314c4.xml";
+        String expressionListFilePath = "../CQLGenerationDocs/GeneratedDocs/elm/expressions/Abdominal_Cramps_314_Expression.txt";
+        runAllExpressionsFromFile(xmlFileName, expressionListFilePath);
         // setup("Patient_is_deceased_84755.xml");
-        generateElmForDebug();
+        //generateElmForDebug();
 
         // results.forEach(object -> Assert.assertTrue(((List<?>) object).size() == 2));
     }
@@ -151,7 +149,7 @@ public class DroolToElmVisitorTest {
         // results.forEach(object -> Assert.assertTrue(((List<?>) object).size() == 2));
     }
 
-    private void runAllExpressionsFromFile(String xmlFileName, String expressionListFilePath) {
+    private List<Object> runAllExpressionsFromFile(String xmlFileName, String expressionListFilePath) {
         setup(xmlFileName);
         List<String> expressions = readFileInList(expressionListFilePath);
         List<Object> results = new ArrayList<Object>();
@@ -164,6 +162,7 @@ public class DroolToElmVisitorTest {
             }
             results.add(result);
         }
+        return results;
     }
 
     private void generateElmForDebug() {
@@ -201,10 +200,10 @@ public class DroolToElmVisitorTest {
         File valuesetIdFile = new File("../CQLGenerationDocs/GeneratedDocs/valueset/valuesetids" + ".txt");
         String json = IOUtil.readFile(valuesetIdFile);
         Gson gson = new Gson();
-        Set<String> ids = gson.fromJson(json, Set.class);
+        // Set<String> ids = gson.fromJson(json, Set.class);
         IBaseBundle bundle = fhirContext.newJsonParser().parseResource(Bundle.class,
                 DroolToElmVisitorTest.class.getResourceAsStream("concepts_full.json"));
-        writeMissingValueSets(gson, ids, bundle);
+        // writeMissingValueSets(gson, ids, bundle);
         registerProviders(fhirContext, bundle);
     }
 
