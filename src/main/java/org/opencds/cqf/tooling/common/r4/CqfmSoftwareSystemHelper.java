248528599178af819c1b3fb7c37f082952d73356
Jreyno77 <jreysteven1@gmail.com>
2021-07-15 17:19:34 -0600

    make r4 createSoftwareSystemDevice public

--- src/main/java/org/opencds/cqf/tooling/common/r4/CqfmSoftwareSystemHelper.java
+++ src/main/java/org/opencds/cqf/tooling/common/r4/CqfmSoftwareSystemHelper.java
@@ -176,7 +176,7 @@ public class CqfmSoftwareSystemHelper extends BaseCqfmSoftwareSystemHelper {
         }
     }
 
-    private Device createSoftwareSystemDevice(CqfmSoftwareSystem system) {
+    public Device createSoftwareSystemDevice(CqfmSoftwareSystem system) {
         Device device = null;
 
         if (this.getSystemIsValid(system)) {
