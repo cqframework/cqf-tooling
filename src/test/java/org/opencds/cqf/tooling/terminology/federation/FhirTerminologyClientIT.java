446fba280a034873427399bd9f2b3f0ce39b4602
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2021-09-13 10:28:31 -0600

    Updates

--- src/test/java/org/opencds/cqf/tooling/terminology/federation/FhirTerminologyClientIT.java
+++ src/test/java/org/opencds/cqf/tooling/terminology/federation/FhirTerminologyClientIT.java
@@ -15,7 +15,7 @@ public class FhirTerminologyClientIT {
 
         // TODO: Need a valid APIKey here
         Endpoint vsacEndpoint = new Endpoint().setAddress("https://uat-cts.nlm.nih.gov/fhir/r4")
-                .addHeader("Authorization: Basic YXBpa2V5OmQ2MmMwOGFmLWNjNGItNGE3ZS1hNzUyLWQ4Yjk4NmM5ZWM4ZQ==");
+                .addHeader("Authorization: Basic dXNlcm5hbWU9O3Bhc3N3b3JkPUluc2VydFlvdXJBUElLZXlIZXJl");
         FhirTerminologyClient ftc = new FhirTerminologyClient(context, vsacEndpoint);
         ValueSet vs = ftc.expand("http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.108.12.1018");
         assertTrue(vs != null);
