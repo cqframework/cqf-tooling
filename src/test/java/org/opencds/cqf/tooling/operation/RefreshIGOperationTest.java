a9488f35725f5fbe0175a356305ff8eadb390d26
Evan Chicoine <evan.chicoine@esacinc.com>
2021-09-20 12:15:55 -0400

    Trimmed valuesets with new method written to determine validity of files based on presence in cql

--- src/test/java/org/opencds/cqf/tooling/operation/RefreshIGOperationTest.java
+++ src/test/java/org/opencds/cqf/tooling/operation/RefreshIGOperationTest.java
@@ -1,14 +1,19 @@
 package org.opencds.cqf.tooling.operation;
 
-
-
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertTrue;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.hl7.fhir.instance.model.api.IBaseResource;
@@ -79,7 +84,7 @@ public class RefreshIGOperationTest {
 
 		// build ini object
 		File iniFile = new File(INI_LOC);
-		String iniFileLocation = iniFile.getPath();
+		String iniFileLocation = iniFile.getAbsolutePath();
 		IniFile ini = new IniFile(iniFileLocation);
 
 		String bundledFilesLocation = iniFile.getParent() + "\\bundles\\measure\\";
@@ -100,51 +105,50 @@ public class RefreshIGOperationTest {
 		for (String measureName : measures.keySet()) {
 			// location of single bundled file:
 			final String bundledFileResult = bundledFilesLocation + measureName + "\\" + measureName + "-bundle.json";
-			System.out.println(bundledFileResult);
 			// multiple individual files in sub directory to loop through:
-			File bundledFiles = new File(bundledFilesLocation + "\\" + measureName + "\\" + measureName + "-files");
-			
-			if (bundledFiles.listFiles() == null) {
-				continue;
-			}
-			
-			// verify files even exist:
-			assertTrue(bundledFiles.listFiles().length > 0);
+			final Path dir = Paths.get(bundledFilesLocation + "\\" + measureName + "\\" + measureName + "-files");
 
 			// loop through each file, determine resourceType and treat accordingly
 			Map<String, String> resourceTypeMap = new HashMap<>();
-			for (File file : bundledFiles.listFiles()) {
-				if (!file.getName().toLowerCase().endsWith(".json")) {
-					continue;
-				}
 
-				Map<?, ?> map = this.jsonMap(file);
-				if (map == null) {
-					System.out.println("# Unable to parse " + file.getName() + " as json");
-					continue;
-				}
+			try (final DirectoryStream<Path> dirStream = Files.newDirectoryStream(dir)) {
+				dirStream.forEach(path -> {
+					File file = new File(path.toString());
 
-				// ensure "resourceType" exists
-				if (map.containsKey(RESOURCE_TYPE)) {
-					String parentResourceType = (String) map.get(RESOURCE_TYPE);
-					// if Library, resource will be translated into "Measure" in main bundled file:
-					if (parentResourceType.equalsIgnoreCase(LIB_TYPE)) {
-						resourceTypeMap.put((String) map.get(ID), MEASURE_TYPE);
-					} else if (parentResourceType.equalsIgnoreCase(BUNDLE_TYPE)) {
-						// file is a bundle type, loop through resources in entry list, build up map of
-						// <id, resourceType>:
-						if (map.get(ENTRY) != null) {
-							ArrayList<Map<?, ?>> entryList = (ArrayList<Map<?, ?>>) map.get(ENTRY);
-							for (Map<?, ?> entry : entryList) {
-								if (entry.containsKey(RESOURCE)) {
-									Map<?, ?> resourceMap = (Map<?, ?>) entry.get(RESOURCE);
-									resourceTypeMap.put((String) resourceMap.get(ID),
-											(String) resourceMap.get(RESOURCE_TYPE));
+					if (file.getName().toLowerCase().endsWith(".json")) {
+
+						Map<?, ?> map = this.jsonMap(file);
+						if (map == null) {
+							System.out.println("# Unable to parse " + file.getName() + " as json");
+						} else {
+
+							// ensure "resourceType" exists
+							if (map.containsKey(RESOURCE_TYPE)) {
+								String parentResourceType = (String) map.get(RESOURCE_TYPE);
+								// if Library, resource will be translated into "Measure" in main bundled file:
+								if (parentResourceType.equalsIgnoreCase(LIB_TYPE)) {
+									resourceTypeMap.put((String) map.get(ID), MEASURE_TYPE);
+								} else if (parentResourceType.equalsIgnoreCase(BUNDLE_TYPE)) {
+									// file is a bundle type, loop through resources in entry list, build up map of
+									// <id, resourceType>:
+									if (map.get(ENTRY) != null) {
+										ArrayList<Map<?, ?>> entryList = (ArrayList<Map<?, ?>>) map.get(ENTRY);
+										for (Map<?, ?> entry : entryList) {
+											if (entry.containsKey(RESOURCE)) {
+												Map<?, ?> resourceMap = (Map<?, ?>) entry.get(RESOURCE);
+												resourceTypeMap.put((String) resourceMap.get(ID),
+														(String) resourceMap.get(RESOURCE_TYPE));
+											}
+										}
+									}
 								}
 							}
 						}
 					}
-				}
+				});
+
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 
 			// map out entries in the resulting single bundle file:
@@ -160,6 +164,95 @@ public class RefreshIGOperationTest {
 			assertTrue(mapsAreEqual(resourceTypeMap, bundledJsonResourceTypes));
 
 		}
+	}
+
+	/**
+	 * Quick method to delete all valuesets not belonging to CQL and identify
+	 * anything missing.
+	 * 
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		boolean deleteExcess = true;
+
+		List<String> listOfValueSets = new ArrayList<>();
+
+		try (final DirectoryStream<Path> dirStream = Files
+				.newDirectoryStream(Paths.get("testfiles/refreshIG/input/cql"))) {
+			dirStream.forEach(path -> {
+				File file = new File(path.toString());
+
+				if (file.getName().toLowerCase().endsWith(".cql")) {
+
+					try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+						String line;
+						boolean valueSetsFound = false;
+						while ((line = br.readLine()) != null) {
+
+							if (line.startsWith("valueset")) {
+								valueSetsFound = true;
+								String vs = line.substring(line.lastIndexOf("/") + 1, line.length()).replace("'", "")
+										.trim();
+								listOfValueSets.add(vs);
+							} else {
+								// done with valuesets section:
+								if (valueSetsFound) {
+									break;
+								}
+							}
+
+						}
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				}
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Map<String, String> valueSetsPresent = new HashMap<>();
+
+		try (final DirectoryStream<Path> dirStream = Files
+				.newDirectoryStream(Paths.get("testfiles/refreshIG/input/vocabulary/valueset/external"))) {
+			dirStream.forEach(path -> {
+				File file = new File(path.toString());
+				String vs = file.getName().replace("valueset-", "").replace(".json", "");
+
+				if (!listOfValueSets.contains(vs)) {
+
+					System.out.println("Valueset found not belonging to any cql: " + path);
+
+					if (deleteExcess == true) {
+						try {
+							Files.delete(path);
+							System.out.println("Deleted " + path);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				} else {
+					valueSetsPresent.put(vs, file.getName());
+				}
+
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		if (valueSetsPresent.keySet().size() < listOfValueSets.size()) {
+			for (String valueSet : listOfValueSets) {
+				if (!valueSetsPresent.containsKey(valueSet)) {
+					System.out.println ("Missing valueset: " + valueSet);
+				}
+			}
+		}
 
 	}
+
 }
