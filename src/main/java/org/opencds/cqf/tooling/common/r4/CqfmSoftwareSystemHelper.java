1637b984828b3f69c65f4104cd00e026e87baa9e
Jreyno77 <jreysteven1@gmail.com>
2021-11-11 18:05:24 -0700

    Revert insignificant Debug logging

--- src/main/java/org/opencds/cqf/tooling/common/r4/CqfmSoftwareSystemHelper.java
+++ src/main/java/org/opencds/cqf/tooling/common/r4/CqfmSoftwareSystemHelper.java
@@ -66,22 +66,15 @@ public class CqfmSoftwareSystemHelper extends BaseCqfmSoftwareSystemHelper {
             Device device = null;
             String deviceOutputPath = getRootDir() + devicePath;
             IOUtils.Encoding deviceOutputEncoding = IOUtils.Encoding.JSON;
-            IOUtils.resourceDirectories.forEach(directory -> System.out.println("ResourceDirectory: " + directory));
-            if (IOUtils.resourceDirectories.isEmpty()) {
-                System.out.println("No resource directories found");
-                IOUtils.printDevicePathsForDebug();
-            }
             for (String path : IOUtils.getDevicePaths(fhirContext)) {
                 DomainResource resourceInPath;
                 try {
                     if (path.endsWith("xml")) {
-                        System.out.println("Parsing Device XML...");
                         deviceOutputEncoding = IOUtils.Encoding.XML;
                         XmlParser xmlParser = (XmlParser)fhirContext.newXmlParser();
                         resourceInPath = (DomainResource) xmlParser.parseResource(new FileReader(new File(path)));
                     }
                     else {
-                        System.out.println("Parsing Device XML...");
                         JsonParser jsonParser = (JsonParser)fhirContext.newJsonParser();
                         resourceInPath = (DomainResource) jsonParser.parseResource(new FileReader(new File(path)));
                     }
