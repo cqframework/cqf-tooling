3b6a77dcb562f769a4b8e3be6438d95ed7f9e5fb
Jacob Percival <jnperciv@gmail.com>
2022-07-06 13:48:16 -0700

    unified fileFhirPlatformParameters argument

--- src/main/java/org/opencds/cqf/tooling/fhir/api/FileFhirPlatformFactory.java
+++ src/main/java/org/opencds/cqf/tooling/fhir/api/FileFhirPlatformFactory.java
@@ -3,5 +3,5 @@ package org.opencds.cqf.tooling.fhir.api;
 import org.opencds.cqf.tooling.parameter.FileFhirPlatformParameters;
 
 public interface FileFhirPlatformFactory {
-  FileFhirPlatform create(FileFhirPlatformParameters fhirPlatformParameters);
+  FileFhirPlatform create(FileFhirPlatformParameters fileFhirPlatformParameters);
 }
