11bfbe61f6ea840c44e713bb73597ff6167b800f
Jacob Percival <jnperciv@gmail.com>
2022-07-08 15:06:20 -0700

    checking file write path

--- src/test/java/org/opencds/cqf/tooling/fhir/api/FileFhirDalTest.java
+++ src/test/java/org/opencds/cqf/tooling/fhir/api/FileFhirDalTest.java
@@ -75,6 +75,7 @@ public class FileFhirDalTest implements CqfmSoftwareSystemTest {
 
     File file = new File(resourceDir + "/Patient/TestPatient.json");
 
+    logger.info(String.format("%s", file.getAbsolutePath()));
     logger.info(String.format("%s", file.exists()));
 
     assertTrue(file.exists());
