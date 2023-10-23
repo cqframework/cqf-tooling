f6336cd715015ecc3ab7d33719ce0c6e7edeba64
Evan Chicoine <evan.chicoine@esacinc.com>
2023-10-23 10:03:51 -0400

    Removing unneeded test from this branch

--- tooling/src/test/java/org/opencds/cqf/tooling/operation/ExtractMatBundleOperationTest.java
+++ tooling/src/test/java/org/opencds/cqf/tooling/operation/ExtractMatBundleOperationTest.java
@@ -208,33 +208,6 @@ public class ExtractMatBundleOperationTest {
     }
 
     @Test
-    public void TestExtractMatBundleWithDirectoryAndSubDirectories() throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        String resourcePath = "org/opencds/cqf/tooling/operation/ExtractMatBundle/bundles_mixed/";
-        URL resourceUrl = classLoader.getResource(resourcePath);
-        if (resourceUrl == null) {
-            throw new IllegalArgumentException("Resource not found: " + resourcePath);
-        }
-
-        File emptyDir = tempFolder.newFolder("bundles");
-        Thread executionThread = new Thread(new Runnable() {
-            public void run() {
-                operation.execute(new String[]{"-ExtractMATBundle", resourceUrl.getFile(), "-dir", "-op=" + emptyDir.getAbsolutePath()});
-            }
-        });
-
-        executionThread.start();
-        try {
-            executionThread.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        File[] files = emptyDir.listFiles();
-        assertNotNull(files);
-        assertTrue(files.length >= 40);
-    }
-
-    @Test
     public void TestExtractMatBundleWithDuplicateBundleXmlJson() throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         String resourcePath = "org/opencds/cqf/tooling/operation/ExtractMatBundle/bundles_duplicate/";
